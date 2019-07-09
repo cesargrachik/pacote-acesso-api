266774ff72b537f0dc46ec6c25a57e0168b21ada
Cesar Grachik <50838423+cesargrachik@users.noreply.github.com>
2019-07-09 19:28:31 -0400

    ALTER SEQUENCE
    ALTER SEQUENCE

---
+++ src/main/resources/db/migration/V08__alterar-sequence-usuaris-perfil-aparelhos.sql
@@ -0,0 +1,5 @@
+ALTER SEQUENCE public.usuarios_id_usuario_seq  RESTART WITH 4;
+
+ALTER SEQUENCE public.perfil_id_perfil_seq RESTART WITH 7;
+
+ALTER SEQUENCE public.aparelhos_id_aparelho_seq RESTART WITH 7;
